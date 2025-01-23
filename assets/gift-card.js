450e0f26a743e79be4af91b84ce7521986526099
Project430 <antoine.schweitz@gmail.com>
2025-01-23 18:18:23 +0100

    Add files via upload

--- assets/gift-card.js
+++ assets/gift-card.js
@@ -1,1 +1,16 @@
-window.giftCard = {}; /* Add gift card specific scripts */
+window.addEventListener('DOMContentLoaded', function() {
+  var qrCode = document.getElementById('QrCode');
+
+  new QRCode(qrCode, {
+    text: qrCode.dataset.identifier,
+    width: 120,
+    height: 120,
+    imageAltText: theme.strings.qrImageAlt
+  });
+
+  document
+    .getElementById('GiftCardDigits')
+    .addEventListener('focus', function(event) {
+      event.target.select();
+    });
+});
